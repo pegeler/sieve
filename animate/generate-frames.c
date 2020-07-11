855a8a74d94beffe12f08aaa9cb7ef26706af93c
pegeler <32426645+pegeler@users.noreply.github.com>
2020-07-11 02:32:39 -0400

    adding copyright notice

--- animate/generate-frames.c
+++ animate/generate-frames.c
@@ -1,3 +1,24 @@
+/*****************************************************************************
+*  FILE: generate-frames.c
+*  PURPOSE: Make animation frames for Sieve of Eratosthenes
+*  DATE: 2020-06-20
+*  AUTHOR: Paul Egeler, MS, GStat
+*
+*  Copyright 2020 Paul Egeler
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*****************************************************************************/
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
